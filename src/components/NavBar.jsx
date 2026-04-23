20cdfadd9a24baa53fe30a351a52ea589e89b51a
kokostahr <starking.nn@gmail.com>
2026-04-23 12:17:17 +0200

    added basic css animations...

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,44 @@
 //this is the navbar, needs to be on every page for navigation
 //FORGOT TO IMPORT LINK FOR NAVIGATION
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 
 
 //function begins
 function NavBar() {
+    //useloction so that the browser knows which page the usar is on for active page
+    const location = useLocation();
+
     return (
         //doing the html scaffolding with links to each page
         //unsure if the homepage should be money snapshot or give a dedicated page? FOR NOW imma do it
         <nav className="nav-bar">
             {/*landing page*/}
-            <Link to="/">Homepage</Link> 
+            <Link to="/"
+                className={location.pathname === "/" ? "nav-active" : ""}>
+                Homepage </Link> 
+            
             {/*other sections*/}
-            <Link to="/moneysnap"> Money Snapshot</Link>
-            <Link to="/strattracks">Strategy Tracks</Link>
-            <Link to="/simLab">Simulation Lab</Link>
-            {/*<Link to="/profile">Profile</Link>*/}
-            <Link to="/auth">SignUp</Link>
+            <Link to="/moneysnap"
+            className={location.pathname === "/moneysnap" ? "nav-active" : ""}>
+                Money Snapshot </Link>
+            
+            <Link to="/strattracks"
+            className={location.pathname === "/strattracks" ? "nav-active" : ""}>
+                Strategy Tracks </Link>
+            
+            <Link to="/simLab"
+            className={location.pathname === "/simLab" ? "nav-active" : ""}>
+                Simulation Lab </Link>
+            
+            <Link to="/auth"
+            className={location.pathname === "/auth" ? "nav-active" : ""}>
+                SignUp </Link>
+
+            {/*<Link to="/profile" 
+            className={location.pathname === "/profile" ? "nav-active" : ""}> 
+            Profile </Link>*/}
+            
         </nav>
     )
 }
